7bde9cdf3b83e05122df373c37e96d37a209f0db
agustina acevedo <agustina.eacevedo1998@gmail.com>
2021-09-15 22:45:24 -0300

    cambio del boton de modo oscuro a claro

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 /*Modo claro/oscuro*/
 const body = document.querySelector('body')
-const botonOscuro = document.getElementById('dark-mode-button')
-const botonClaro = document.getElementById('light-mode-button')
+const botonToggle = document.getElementById('toggle-mode-button')
+const iconToggle = document.getElementById('icon-toggle-mode')
+const textToggle = document.getElementById('text-toggle')
 
-const changeMode = () =>{
+const changeMode = () => {
+    // se hace toggle de todas las clases que necesitamos cambiar
     body.classList.toggle('dark-mode')
     body.classList.toggle('light-mode')
-    botonClaro.toggle('botonOscuro')
-    
+    iconToggle.classList.toggle('far')
+    iconToggle.classList.toggle('fas')
+    //chequeamos con modeStatus si el estado es dark o light. Si hay un dark-mode, 
+    // esta en modo dark, de lo contrario es light
+    const modeStatus = document.getElementsByClassName('dark-mode');
+    // Si el modo es DARK vamos a pasar a modo claro
+    if (modeStatus.length > 0) { // AQUI NOS FIJAMOS CUANTOS ELEMENTOS se obtuvieron con getELementsByClassName
+       console.log("ESTA EN DARK MODE");
+       textToggle.textContent = "Modo Claro"
+    // Si el modo es LIGH vamos a pasar a modo oscuro 
+    } else {
+        console.log("Esta en light mode")
+        textToggle.textContent = "Modo Oscuro"
+    }
+    // OPCIONAL DESCOMENTAMOS ESTA LINEA Y TENEMOS UN SHORTCUT PARA EL IF 
+    // textToggle.textContent = modeStatus.length > 0 ? "Modo Claro" : "Modo Oscuro"
 }
-botonOscuro.addEventListener('click', changeMode)
-botonClaro.addEventListener('click', changeMode)
+botonToggle.addEventListener('click', changeMode)
